fea0db012a6450125dcab9e2f1eb8c9ff4507c89
weswhitney <wesleywhitney@gmail.com>
2022-02-15 11:16:16 -0700

    feat: signin and impact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import QRBox from './QRBox';
+import QRBox from './routes/QRBox';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <QRBox />
+      <h1>Flex Alert</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/">Signin</Link> |{" "}
+        <Link to="impact">Impact</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
