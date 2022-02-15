43a85015250ae0f4dfcedd28872ee37a22f94bbb
weswhitney <wesleywhitney@gmail.com>
2022-02-14 20:21:59 -0700

    chore: stub out user for when we get it back

--- src/QRBox.tsx
+++ src/QRBox.tsx
@@ -1,11 +1,25 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Box, ThemeProvider } from '@mui/system'
 import QRCode from "react-qr-code";
 
+const getUserInfo = async () => {
+  const response = await fetch(`https://damp-journey-92337.herokuapp.com/url`)
+  const respJson = await response.json();
+  const name = respJson[0];
 
+  return name;
+}
 
 export default function QRBox() {
-  const codeInfo = `mycolorado://share?Destination=${process.env.REACT_APP_DESTINATION_ID}&ControlCode=12345&Message=thanks+for+id+with-eth-denver-hack-app&callbackUrl=${process.env.REACT_APP_WEBHOOK_URL}`
+  const [user, setUser] = useState();
+  const codeInfo = `mycolorado://share?Destination=${process.env.REACT_APP_DESTINATION_ID}&Message=thanks+for+id+with-eth-denver-hack-app&callbackUrl=${process.env.REACT_APP_WEBHOOK_URL}`
+
+  React.useEffect(() => {
+    (async () => {
+      const username = await getUserInfo();
+      setUser(username)
+    })()
+  }, [])
 
   return (
     <ThemeProvider
@@ -30,6 +44,19 @@ export default function QRBox() {
         }}
       >
         <QRCode value={codeInfo} />
+      </Box>
+      <Box
+        sx={{
+          width: 300,
+          height: 300,
+          bgcolor: 'primary.main',
+          '&:hover': {
+            backgroundColor: 'primary.dark',
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
+      >
+        <div>hello {user}</div>
       </Box>
     </ThemeProvider>
   );
